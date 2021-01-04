4b4d3def5d47ee2bed3ffd65445ccee7396ca35f
Elisson Alves Silva <elisson@192.168.1.10>
2021-01-03 20:27:19 -0400

    test: ensure account validator on database

--- src/infra/postgres/account/account-validator-repository/account.spec.ts
+++ src/infra/postgres/account/account-validator-repository/account.spec.ts
@@ -1,5 +1,16 @@
+import { getConnection } from 'typeorm'
+
+import { Account } from '@/infra/postgres/account/models/Account'
 import { PostgresHelper } from '@/infra/postgres/helpers/postgres-helper'
 import { AccountValidatorDatabaseRepository } from '@/infra/postgres/account/account-validator-repository/account'
+
+const makeFakeAccount = (): object => {
+  return {
+    accountNumber: 123,
+    type: 1,
+    balance: 100
+  }
+}
 
 const makeSut = (): AccountValidatorDatabaseRepository => {
   return new AccountValidatorDatabaseRepository()
@@ -16,8 +27,20 @@ describe('Account Validator Repository', () => {
 
   it('Should return true if exist a account', async () => {
     const sut = makeSut()
+    await getConnection()
+      .createQueryBuilder()
+      .insert()
+      .into(Account)
+      .values(makeFakeAccount())
+      .execute()
 
     const isValid = await sut.isValid(123)
-    console.log(isValid)
+    expect(isValid).toBeTruthy()
+  })
+  it('Should return false if not exist a account', async () => {
+    const sut = makeSut()
+
+    const isValid = await sut.isValid(1234)
+    expect(isValid).toBe(false)
   })
 })
