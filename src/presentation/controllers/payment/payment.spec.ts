79f7079b319201e9044401157550cdc7f74e8f3e
Elisson Alves Silva <elisson@192.168.1.10>
2021-01-04 18:53:32 -0400

    test: integrate do payment

--- src/presentation/controllers/payment/payment.spec.ts
+++ src/presentation/controllers/payment/payment.spec.ts
@@ -2,26 +2,27 @@ import { AccountValidator } from '@/presentation/protocols'
 import { MissingParamError, InvalidParamError } from '@/presentation/erros'
 import { badRequest, successResponse } from '@/presentation/helpers/http-helper'
 import { PaymentController } from '@/presentation/controllers/payment/payment'
-// import { AccountModel } from '@/domain/models/deposit/Account'
+import { AccountModel } from '@/domain/models/account/Account'
+import { DoPayment, DoPaymentModel } from '@/domain/usecases/payment/DoPayment'
 
-// const fakeDepositResponse: AccountModel = {
-//   id: 'abc',
-//   accountNumber: 1,
-//   type: 1,
-//   balance: 110,
-//   createdAt: new Date('2020-01-01'),
-//   updatedAt: new Date('2020-01-01')
-// }
+const fakePaymentResponse: AccountModel = {
+  id: 'abc',
+  accountNumber: 1,
+  type: 1,
+  balance: 20,
+  createdAt: new Date('2020-01-01'),
+  updatedAt: new Date('2020-01-01')
+}
 
-// const makeDoPayment = (): DoPayment => {
-//   class DoPaymentStub implements DoPayment {
-//     async deposit (deposit: DoPaymentModel): Promise<AccountModel> {
-//       return new Promise(resolve => resolve(fakeDepositResponse))
-//     }
-//   }
+const makeDoPayment = (): DoPayment => {
+  class DoPaymentStub implements DoPayment {
+    async payment (payment: DoPaymentModel): Promise<AccountModel> {
+      return new Promise(resolve => resolve(fakePaymentResponse))
+    }
+  }
 
-//   return new DoPaymentStub()
-// }
+  return new DoPaymentStub()
+}
 
 const makeAccountValidator = (): AccountValidator => {
   class AccountValidatorStub implements AccountValidator {
@@ -36,12 +37,14 @@ const makeAccountValidator = (): AccountValidator => {
 interface SutType {
   sut: PaymentController
   accountValidatorStub: AccountValidator
+  doPaymentStub: DoPayment
 }
 
 const makeSut = (): SutType => {
   const accountValidatorStub = makeAccountValidator()
-  const sut = new PaymentController(accountValidatorStub)
-  return { sut, accountValidatorStub }
+  const doPaymentStub = makeDoPayment()
+  const sut = new PaymentController(accountValidatorStub, doPaymentStub)
+  return { sut, accountValidatorStub, doPaymentStub }
 }
 
 describe('Payment Controller', () => {
@@ -165,8 +168,8 @@ describe('Payment Controller', () => {
       expect(httResponse).toEqual(badRequest(new InvalidParamError('accountOrigin')))
     })
 
-    it('Should return 200 if all values is provided', async () => {
-      const { sut } = makeSut()
+    it('Should return 200 if all values is provided and it was make a payment', async () => {
+      const { sut, doPaymentStub } = makeSut()
 
       const lastValue = 10
       const httpRequest = {
@@ -178,31 +181,13 @@ describe('Payment Controller', () => {
           value: lastValue
         }
       }
+      const actualValue = fakePaymentResponse.balance - lastValue
+      Object.assign(fakePaymentResponse, { balance: actualValue })
+
+      jest.spyOn(doPaymentStub, 'payment').mockReturnValueOnce(new Promise(resolve => resolve(fakePaymentResponse)))
 
       const httResponse = await sut.handle(httpRequest)
-      expect(httResponse).toEqual(successResponse({}))
+      expect(httResponse).toEqual(successResponse(fakePaymentResponse))
     })
-
-    // it('Should return 200 if all values is provided and it was make a deposit', async () => {
-    //   const { sut } = makeSut()
-
-    //   const lastValue = 10
-    //   const httpRequest = {
-    //     body: {
-    //       accountOrigin: 10000,
-    //       accountDestination: 90000,
-    //       createdDate: new Date('2020-09-09 15:30:30'),
-    //       sendDate: new Date('2020-09-09 15:30:30'),
-    //       value: lastValue
-    //     }
-    //   }
-    //   const actualValue = fakeDepositResponse.balance + lastValue
-    //   Object.assign(fakeDepositResponse, { balance: actualValue })
-
-    //   jest.spyOn(doDepositStub, 'deposit').mockReturnValueOnce(new Promise(resolve => resolve(fakeDepositResponse)))
-
-    //   const httResponse = await sut.handle(httpRequest)
-    //   expect(httResponse).toEqual(successResponse(fakeDepositResponse))
-    // })
   })
 })
