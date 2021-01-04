caaf4042f2252525e92c7e6c2d252587885e72b7
Elisson Alves Silva <elisson@192.168.1.10>
2021-01-03 22:27:14 -0400

    test: ensure deposit infra to update value of balance

---
+++ src/infra/postgres/deposit/deposit.spec.ts
@@ -0,0 +1,66 @@
+import { Account } from '@/infra/postgres/account/entities/Account'
+import { getConnection } from 'typeorm'
+import { PostgresHelper } from '@/infra/postgres/helpers/postgres-helper'
+import { DoDepositDatabaseRepository } from '@/infra/postgres/deposit/deposit'
+import { DoDepositModel } from '@/domain/usecases/deposit/DoDeposit'
+
+const lastBalance = 100
+
+const makeFakeDepositAccount = (): DoDepositModel => {
+  return {
+    accountOrigin: 1,
+    accountDestination: 2,
+    createdDate: new Date('2020-01-01'),
+    sendDate: new Date('2020-01-01'),
+    value: 10
+  }
+}
+
+const makeFakeAccount = (): object => {
+  return {
+    accountNumber: 2,
+    type: 1,
+    balance: lastBalance
+  }
+}
+
+const makeSut = (): DoDepositDatabaseRepository => {
+  return new DoDepositDatabaseRepository()
+}
+
+describe('Do Deposit Database Repository', () => {
+  beforeAll(async () => {
+    await PostgresHelper.connect()
+  })
+
+  afterAll(async () => {
+    await PostgresHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    await PostgresHelper.clear()
+  })
+
+  it('Should deposit change value with a deposit', async () => {
+    const sut = makeSut()
+    await getConnection()
+      .createQueryBuilder()
+      .insert()
+      .into(Account)
+      .values(makeFakeAccount())
+      .execute()
+
+    const fakeDepositAccount = makeFakeDepositAccount()
+    await sut.deposit(fakeDepositAccount)
+
+    const response = await getConnection()
+      .getRepository(Account)
+      .createQueryBuilder('account')
+      .where('account.accountNumber = :accountNumber', { accountNumber: fakeDepositAccount.accountDestination })
+      .getOne()
+
+    const newBalance = fakeDepositAccount.value + lastBalance
+
+    expect(response.balance).toEqual(newBalance)
+  })
+})
